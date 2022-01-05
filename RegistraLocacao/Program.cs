9f6d8705f659256115135f0c5b41ea2389df3dbc
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-05 16:17:22 -0300

    Implementacao da entrada dos dados para instanciacao do objeto CarRental no arquivo program.

--- RegistraLocacao/Program.cs
+++ RegistraLocacao/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RegistraLocacao.Entities;
+using System.Globalization;
 
 namespace RegistraLocacao
 {
@@ -6,7 +8,16 @@ namespace RegistraLocacao
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd,MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd,MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
         }
     }
 }
